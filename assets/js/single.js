6c6a1c323bc48296d33109e845b877a2d35782f9
Pure KaoZ <nickolaus.carter81@gmail.com>
2021-01-16 14:09:55 -0600

    view repo list and error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  
+  if (repoName) {
+    repoNameEl.textContent = repoName
+    
+    getRepoIssues(repoName)
+  } else {
+    document.location.replace("./index.html")
+  }
+};
 
 var getRepoIssues = function(repo) {
   var apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
@@ -9,8 +24,13 @@ var getRepoIssues = function(repo) {
       response.json().then(function(data) {
         displayIssues(data);
       });
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
     } else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -20,6 +40,7 @@ var displayIssues = function(issues) {
     issueContainerEl.textContent = "This repo has no open issues!";
     return;
   }
+
   for (let i = 0; i < issues.length; i++) {
     // create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
@@ -46,4 +67,16 @@ var displayIssues = function(issues) {
   }
 };
 
-getRepoIssues("facebook/react")
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
